d4bcb31c8f6a119496302988a1ff7acec0f72010
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-10-15 23:49:06 -0300

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,28 @@
+public class Paciente {
+    String nome; 
+     int datanasciemento;
+     String sexo; 
+     String alergia; 
+     String tipo sanguineo; 
+    
+    
+    Paciente.p1 = new Paciente()
+    p1.nome = "João ";
+    p1.datanasciemento=12/05/2000
+    p1.sexo=  "Masculino";
+    p1.alergia="a pó ";
+    p1. tipo sanguineo="O+";
+    
+    System.out.println =("Nome do Paciente:  "+p1.nome "Data de nasciemento: "+ p1.datanasciemento ,"Sexo:" + p1.sexo ,"Alergia:" + p1.alergia , "Tipo sanguineo:" + p1.tipo sanguineo) ;
+    
+    Mae.dop1 =new Mae()
+    mae.nome = "Maria";
+    mae.datanasciemento= 30/02/1989 ;
+    mae. sexo = "Feminino";
+    mae.alergia="A pó";
+    mae.tipo sanguineo="AB";
+  
+     System.out.println =("Nome do Paciente:  "mae .nome "Data de nasciemento: "+ mae.datanasciemento ,"Sexo:" + mae.sexo ,"Alergia:" + mae.alergia , "Tipo sanguineo:" + mae.tipo sanguineo) ;  
+    
+    
+}
